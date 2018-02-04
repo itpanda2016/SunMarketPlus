08897977411dd1d1c7a7916dc5399eb7dda9e8db
熊林 <SFROST@HOME-10>
2018-02-04 16:48:21 +0800

    后台Session搞定

---
+++ Shop/MobileWeiXin/Controllers/AuthenticationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MobileWeiXin.Controllers {
+    public class AuthenticationController : ActionFilterAttribute {
+        public override void OnActionExecuting(ActionExecutingContext filterContext) {
+            if (filterContext.HttpContext.Session["loginUser"] == null)
+                filterContext.Result = new RedirectResult("/Management/Login");
+            //filterContext.Result = new RedirectToRouteResult("/Login", new RouteValueDictionary { { "from", filterContext.HttpContext.Request.Url.ToString() } });
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
---
+++ Shop/MobileWeiXin/Controllers/ManagementController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MobileWeiXin.Controllers
+{
+    public class ManagementController : Controller
+    {
+        // GET: Management
+        [AuthenticationController]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login() {
+            if (!string.IsNullOrEmpty(Request["loginPassword"])) {
+                Session["loginUser"] = Request["loginPassword"];
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
